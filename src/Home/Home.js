bdc13b419e0ffc2a32c0b409ec852f0ce2abc238
Azman <azman95ict@gmail.com>
2019-03-13 11:35:29 +0800

    home pages with logo

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,25 +1,12 @@
-import React, { Component } from 'react';
-import logo from './../logo.svg';
-import './../App.css';
+import React, { Component } from "react";
+import logo from "./../img/pinvent-logo.png";
+import "./../App.css";
 
 class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to refresh.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="home">
+        <img src={logo} alt="logo" className="logo" />
       </div>
     );
   }
